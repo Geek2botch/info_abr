eb0b3aec2cae2ce53d079d974dc3a014ed27efe5
quentin.nouguier <q.nouguier98@gmail.com>
2022-04-19 20:43:45 +0200

    balance added

--- AVL_Node.py
+++ AVL_Node.py
@@ -30,6 +30,14 @@ class AVL_Node:
         head._right = self
         self._left = temp
         return head
+    
+
+    def new_balance(self, delta):
+        self._balance += delta
+        if 1 >= self._balance >= -1:
+            return self
+        else:
+            return self.node_balance()
 
     def insert_(self, value):
         if self._value == value:
@@ -38,15 +46,31 @@ class AVL_Node:
         if value < self._value:
             if self._left is None:
                 self._left = AVL_Node(value)
-                return self
+                return self.new_balance(1)
             else:
-                return None if self._left.insert(value) is None else self
+                old_bal = self._left._balance
+                node = self._left.insert(value)
+                if node is None:
+                    return None
+                else:
+                    self._left = node
+                    if old_bal == 0 and old_bal != node._balance:
+                        node = self.new_balance(1)
+                return node
         else:
             if self._right is None:
                 self._right = AVL_Node(value)
-                return self
+                return self.new_balance(1)
             else:
-                return None if self._right.insert_(value) is None else self
+                old_bal = self._right._balance
+                node = self._right.insert(value)
+                if node is None:
+                    return None
+                else:
+                    self._right = node
+                    if old_bal == 0 and old_bal != node._balance:
+                        node = self.new_balance(1)
+                return node
 
     def insert(self, value):
         node = self.insert_(value)
