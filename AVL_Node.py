a4f7f308b8f054cb14594619454c9acc089545fa
quentin.nouguier <q.nouguier98@gmail.com>
2022-04-19 19:16:48 +0200

    rot added

--- AVL_Node.py
+++ AVL_Node.py
@@ -15,17 +15,21 @@ class AVL_Node:
 
     def rot_left(self):
         global NB_ROT
-        self._right._left = self
-        self._right = None
         NB_ROT += 1
-        return self._right, 0
+        head = self._right
+        temp = head._left
+        head._left = self
+        self._right = temp
+        return head
 
     def rot_right(self):
         global NB_ROT
-        self._left._right = self
-        self._left = None
         NB_ROT += 1
-        return self._left, 0
+        head = self._left
+        temp = head._right
+        head._right = self
+        self._left = temp
+        return head
 
     def insert_(self, value):
         if self._value == value:
@@ -45,7 +49,7 @@ class AVL_Node:
                 return None if self._right.insert_(value) is None else self
 
     def insert(self, value):
-        noeud = self.insert_(value)
-        return self if noeud is None else noeud
+        node = self.insert_(value)
+        return self if node is None else node
 
 
